91205ac4e34ddd86f0e0fa808df88ea60f24fe36
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-18 00:26:41 +0900

    とりあえず完成　レビュー待ち

---
+++ src/components/Atoms/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../variables/color";
+import TEXT from "../../variables/texts";
+
+const Title = () => {
+  return (
+    <div>
+      <StyledTextWide>SIMPLE TODO APP</StyledTextWide>
+      <StyledTextNarrow>SIMPLE TODO APP</StyledTextNarrow>
+    </div>
+  );
+};
+export default Title;
+
+const StyledTextWide = styled.div`
+  color: ${COLOR.WHITE};
+  ${TEXT.L}
+`;
+const StyledTextNarrow = styled.div`
+  color: ${COLOR.WHITE};
+  ${TEXT.M}
+`;
