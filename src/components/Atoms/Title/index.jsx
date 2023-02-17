3b7c33bc2f8820a261d69f20debcfaecda48b4c0
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-18 00:38:34 +0900

    ディレクトリを変更

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import COLOR from "../../variables/color";
-import TEXT from "../../variables/texts";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
 
 const Title = () => {
   return (
