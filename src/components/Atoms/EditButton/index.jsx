f6a0dd60064210d7019c6b6d11a81dfac2688454
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-18 14:22:05 +0900

    鉛筆マークの表示

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import pencil from "../../../assets/svg/pencil.svg";
 
-const SampleButton = ({ onClick }) => {
-  // return <button onClick={onClick}>ボタン</button>;
-  return <img src={pencil} />;
+const SampleButton = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.label}
+      <img src={pencil} />
+    </button>
+  );
 };
 export default SampleButton;
