1aa400365c01a73c7236840e1f40a5f03b24dd12
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-18 17:01:03 +0900

    背景を追加

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -6,6 +6,7 @@ import COLOR from "../../../variables/color";
 const SampleButton = (props) => {
   return (
     <Button onClick={props.onClick}>
+      <Circle></Circle>
       <Img src={pencil} />
     </Button>
   );
@@ -16,10 +17,25 @@ const Button = styled.button`
   border: none;
 `;
 const Img = styled.img`
-  ${Img}:hover {
+  color: ${COLOR.LIGHT_GRAY};
+`;
+const Circle = styled.div`
+  ${Circle}:hover {
     background-color: ${COLOR.LIGHT_GRAY};
+    width: 25px;
+    height: 25px;
     opacity: 0.2;
-    border-radius:50%;
+    border-radius: 50%;
+    position: absolute;
+    left: 21px;
+  }
+  ${Circle} {
+    width: 25px;
+    height: 25px;
+    position: absolute;
+    left: 21px;
+    background-color: transparent;
+  }
 `;
 
 export default SampleButton;
