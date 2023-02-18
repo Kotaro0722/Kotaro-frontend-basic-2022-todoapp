e59a9ac46ad4e51b66e37c97e0ed9f8f08a94c86
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-18 16:13:15 +0900

    ボタンの背景の透明化

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import pencil from "../../../assets/svg/pencil.svg";
+import COLOR from "../../../variables/color";
 
 const SampleButton = (props) => {
   return (
-    <button onClick={props.onClick}>
-      {props.label}
-      <img src={pencil} />
-    </button>
+    <Button onClick={props.onClick}>
+      <Img src={pencil} />
+    </Button>
   );
 };
+
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+`;
+const Img = styled.img`
+  ${Img}:hover {
+    background-color: ${COLOR.LIGHT_GRAY};
+    opacity: 0.2;
+    border-radius:50%;
+`;
+
 export default SampleButton;
