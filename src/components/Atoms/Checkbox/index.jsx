4786aafbb6981156f75f861da32f335baff85660
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-19 00:12:55 +0900

    大体完成　レビュー待ち

---
+++ src/components/Atoms/Checkbox/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import check from "../../../assets/svg/check.svg";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+
+const Checkbox = (props) => {
+  return (
+    <Box>
+      <Check src={check} />
+    </Box>
+  );
+};
+const Check = styled.img`
+  display: none;
+  width: 16px;
+  height: 16px;
+  }
+`;
+
+const Box = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 2px solid ${COLOR.LIGHT_GRAY};
+  border-radius: 2px;
+  cursor: pointer;
+  ${Box}:hover ${Check}{
+    display: block;
+`;
+
+export default Checkbox;
