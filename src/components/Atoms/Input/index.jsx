4e1e8685414205b7fdc5e7e1007f761f260e4b11
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-21 01:47:44 +0900

    Ref,Effect 1.は終了

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
 
 const Input = (onEditComplete, defaultValue) => {
-  return <Box></Box>;
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current = defaultValue;
+  });
+  return <styledInput ref={inputRef}>{inputRef.current}</styledInput>;
 };
 
-const Box = styled.input`
+const styledInput = styled.input`
   background-color: transparent;
   border: none;
   outline: none;
   border-radius: 2px;
   color: ${COLOR.LIGHT_GRAY};
-  ${Box}:focus {
+  ${styledInput}:focus {
     background-color: ${COLOR.BLACK};
   }
 `;
