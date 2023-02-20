277003771f446aa50e6a580aff56e8dc2bb89aab
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-02-21 02:37:55 +0900

    微調整　styled.input``の名前によってなぜか表示されたりされなかったりする

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -2,21 +2,21 @@ import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
 
-const Input = ({ onEditComplete, defaultValue }) => {
+const Input = (onEditComplete, defaultValue) => {
   const inputRef = useRef();
   useEffect(() => {
-    inputRef.current.innerHTML = defaultValue;
+    inputRef.current.value = defaultValue;
   }, []);
-  return <styledInput ref={inputRef}></styledInput>;
+  return <Box ref={inputRef}></Box>;
 };
 
-const styledInput = styled.input`
+const Box = styled.input`
   background-color: transparent;
   border: none;
   outline: none;
   border-radius: 2px;
   color: ${COLOR.LIGHT_GRAY};
-  ${styledInput}:focus {
+  ${Box}:focus {
     background-color: ${COLOR.BLACK};
   }
 `;
