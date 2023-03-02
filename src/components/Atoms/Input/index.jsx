0df254f60d7699396474fa00d247d63cd95e6af7
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-03-02 19:47:06 +0900

    修正

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
 
 const Input = ({ onEditComplete, defaultValue }) => {
   const inputRef = useRef();
@@ -27,11 +28,12 @@ const Input = ({ onEditComplete, defaultValue }) => {
 
 const StyledInput = styled.input`
   background-color: ${COLOR.BLACK};
-  border: solid ${COLOR.BLACK};
+  border: none;
   outline: none;
   border-radius: 2px;
   color: ${COLOR.LIGHT_GRAY};
   padding-left: 4px;
+  ${TEXT.S}
 `;
 
 export default Input;
