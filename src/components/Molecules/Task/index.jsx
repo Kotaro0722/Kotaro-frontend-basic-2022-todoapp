ab17b34ee3cabe1d1ec4d7d30885f1cf68f2f3fc
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-03-17 16:48:04 +0900

    機能は完成

--- src/components/Molecules/Task/index.jsx
+++ src/components/Molecules/Task/index.jsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import CheckBox from "../../Atoms/Checkbox/index.jsx";
 import Input from "../../Atoms/Input/index.jsx";
 import EditButton from "../../Atoms/EditButton/index";
+import COLOR from "../../../variables/color.js";
 
 const Task = ({ onTaskChange, onTaskComplete, taskName, defaultIsEditing }) => {
   const [isEditing, setIsEditing] = useState(defaultIsEditing);
   const onEditComplete = (value) => {
     setIsEditing(false);
     () => onTaskChange(value);
-    console.log(isEditing);
+    console.log(value);
   };
   const onEditButtonClick = () => {
     setIsEditing(true);
@@ -20,21 +21,26 @@ const Task = ({ onTaskChange, onTaskComplete, taskName, defaultIsEditing }) => {
       <StyledCheckBoxWrapper>
         <CheckBox onClick={onTaskComplete}></CheckBox>
       </StyledCheckBoxWrapper>
-      <Input onEditComplete={taskName} defaultValue={taskName}></Input>
+      {/* <Input onEditComplete={taskName} defaultValue={taskName}></Input>
       <StyledNameAndButtonWrapper>
         <StyledTaskName>{taskName}</StyledTaskName>
         <StyledEditButtonWrapper>
           <EditButton onEditButtonClick></EditButton>
         </StyledEditButtonWrapper>
-      </StyledNameAndButtonWrapper>
-
-      {/* (isEditing ? (<Input onEditComplete={taskName}></Input>):(<StyledNameButtonWrapper>
+      </StyledNameAndButtonWrapper> */}
+      {isEditing ? (
+        <Input
+          onEditComplete={() => onEditComplete(taskName)}
+          defaultValue={taskName}
+        ></Input>
+      ) : (
+        <StyledNameAndButtonWrapper>
           <StyledTaskName>{taskName}</StyledTaskName>
           <StyledEditButtonWrapper>
-            <EditButton onClick={onEditButtonClick()}></EditButton>
+            <EditButton onClick={onEditButtonClick}></EditButton>
           </StyledEditButtonWrapper>
-        </StyledNameButtonWrapper>)
-      ) */}
+        </StyledNameAndButtonWrapper>
+      )}
       {/* if (isEditing == true) {
         <Input onEditComplete={taskName}></Input>
       } else {
@@ -64,5 +70,6 @@ const StyledNameAndButtonWrapper = styled.div`
 `;
 const StyledTaskName = styled.div`
   text-align: center;
+  color: ${COLOR.LIGHT_GRAY};
 `;
 const StyledEditButtonWrapper = styled.div``;
