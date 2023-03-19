4a39384f480f274c3f369b9a0fad3f9cdd089cd4
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-03-19 15:03:38 +0900

    とりあえず完成

--- src/components/Pages/MainPages/index.jsx
+++ src/components/Pages/MainPages/index.jsx
@@ -1,1 +1,22 @@
 import React from "react";
+import Title from "../../Atoms/Title/index.jsx";
+import TodoCard from "../../Organisms/TodoCard";
+import styled from "styled-components";
+
+const Page = () => {
+  return (
+    <PageWrapper>
+      <Title />
+      <TodoCard />
+    </PageWrapper>
+  );
+};
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
+`;
+
+export default Page;
