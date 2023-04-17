3fdbb50586a8ef0e305df55acdf9b92937743680
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-04-17 21:28:18 +0900

    AlertManagerとりあえず完成

--- src/components/Organisms/AlertManager/index.jsx
+++ src/components/Organisms/AlertManager/index.jsx
@@ -1,4 +1,5 @@
 import { useAlertHandlerContext } from "path/to/contexts/alert_handler";
+import React, { useState } from "react";
 
 /* コンポーネント関数内 */
 //Contextを取得
@@ -6,4 +7,11 @@ const AlertHandlerContext = useAlertHandlerContext();
 
 //AlertHandlerContextから直接値が取り出せる
 console.log(AlertHandlerContext.visible);
-AlertHandlerContext.setAlert("message"); //Alertの表示
+AlertHandlerContext.setAlert("タスクの名前が設定されていません。"); //Alertの表示
+setInterval(() => {
+  const [timer, setTimer] = useState(0);
+  setTimer(timer++);
+  if (timer > 5) {
+    AlertHandlerContext.closeAlert();
+  }
+});
