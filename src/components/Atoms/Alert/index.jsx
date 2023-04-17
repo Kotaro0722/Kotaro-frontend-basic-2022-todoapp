8e1a0e6e5eaca783665101423c123e9cb535688e
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-04-17 20:35:18 +0900

    アラート本体の作成完了

--- src/components/Atoms/Alert/index.jsx
+++ src/components/Atoms/Alert/index.jsx
@@ -1,9 +1,25 @@
 import styled from "styled-components";
+import COLOR from "../../../variables/color";
 
 const Alert = (isActive) => {
-  return <AlertComponent isActive={isActive}></AlertComponent>;
+  return (
+    <AlertComponent isActive={isActive}>
+      タスクの名前が設定されていません。
+    </AlertComponent>
+  );
 };
 
 const AlertComponent = styled.div`
   display: ${(props) => (props.isActive ? "block" : "none")};
+  padding: 10px 20px;
+  gap: 10px;
+  width: 400px;
+  height: 40px;
+  background-color: ${COLOR.RED};
+  color: ${COLOR.WHITE};
+  font-size: 14px;
+  display: flex;
+  border-radius: 4px;
 `;
+
+export default Alert;
